83f7a44f4a3bab67a4a18715b821eb88881d3622
Admiral-RDX <jano.falosny@gmail.com>
2024-10-03 06:54:47 +0200

    Main + Style

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module go-templ-dev
 
 go 1.23.1
 
-require github.com/a-h/templ v0.2.778
+require (
+	github.com/a-h/templ v0.2.778
+	github.com/gorilla/mux v1.8.1
+)
--- main.go
+++ main.go
@@ -12,17 +12,21 @@ import (
 )
 
 func main() {
+	// Template definition
 	loginPage := base.HTML(login.Login().Render)
 	dashboardPage := base.HTML(dashboard.Dashboard().Render)
 	calendarPage := base.HTML(calendar.Calendar().Render)
 
+	// Static files
 	fs := http.FileServer(http.Dir("static"))
 	http.Handle("/static/", http.StripPrefix("/static/", fs))
 
+	// Routes
 	http.Handle("/", templ.Handler(loginPage))
 	http.Handle("/dashboard", templ.Handler(dashboardPage))
 	http.Handle("/calendar", templ.Handler(calendarPage))
 
+	// HTTP
 	fmt.Println("Listening on :3000")
 	http.ListenAndServe(":3000", nil)
 }
